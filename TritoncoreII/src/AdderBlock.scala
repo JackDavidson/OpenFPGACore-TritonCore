6603c46233d3860f8ee20834b521d77d15592084
JackDavidson <jack.davidson38@gmail.com>
2016-07-02 19:09:16 -0700

    Started work on TritoncoreII

---
+++ TritoncoreII/src/AdderBlock.scala
@@ -0,0 +1,41 @@
+import Chisel._
+
+
+class AdderBlock extends Module {
+  val io = new Bundle {
+    val carryIn     = Bits(INPUT,  1)
+    val inputA      = Bits(INPUT,  8) // the first input number
+    val inputB      = Bits(INPUT,  8) // the second input number
+    val result      = Bits(OUTPUT, 8) // the addition result
+    val carryOut    = Bits(OUTPUT, 1)
+  }
+
+  // the following line is weird. We need a carry-in and a carry-out. Hopefully verilog will optomize this.
+  val additionResult = UInt(0, width = 9) + io.inputA + io.inputB + io.carryIn
+  io.result := additionResult(7,0);
+  io.carryOut := additionResult(8);
+}
+
+class AdderBlockTests(c: AdderBlock) extends Tester(c) {
+  poke(c.io.inputA, 0xAA)
+
+  var valuesWithResults = Array(Array(0,0,0xAA,0), Array(1,0,0xAB,0), Array(0,2,0xAC,0), Array(1,3,0xAE,0),
+    Array(0,0x90,0x3A,1))
+
+  for (valueWithResult <- valuesWithResults) {
+    poke(c.io.carryIn, valueWithResult(0))
+    poke(c.io.inputB, valueWithResult(1))
+    expect(c.io.result, valueWithResult(2))
+    expect(c.io.carryOut, valueWithResult(3))
+  }
+}
+
+object AdderBlockTestRunner {
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(Array[String]("--backend", "c", "--compile", "--test", "--genHarness"),
+      () => Module(new AdderBlock()))
+    {
+      c => new AdderBlockTests(c)
+    }
+  }
+}
---
+++ TritoncoreII/src/LogicBlock.scala
@@ -0,0 +1,50 @@
+import Chisel._
+
+
+class LogicBlock extends Module {
+  val io = new Bundle {
+    val programmedInputs = Bits(INPUT,  16) // This data is provided by the GlobalProgrammer
+    val enableFlipflop   = Bits(INPUT,  1 ) // Enables or disables the flipflop
+    val flipflopReset    = Bits(INPUT,  1 ) // The value which the flipflop gets reset to
+    val selectInputs     = Bits(INPUT,  4 ) // the 4 select inputs for the LUT
+    val reset            = Bits(INPUT,  1 ) // the reset signal for the reg
+    val result           = Bits(OUTPUT, 1 ) // the 1-bit output
+  }
+
+  val lookupTable = Module(new LUT())
+  lookupTable.io.lut := io.programmedInputs  // the first 16 inputs are the values we select from
+  lookupTable.io.sel := io.selectInputs      // the input to the
+
+  val optionalReg = Reg(init = UInt(0, width = 1)) // reg on the end of the lookup table
+  optionalReg := Mux(io.reset(0), io.flipflopReset, lookupTable.io.res)  // when resetting, we select from the reset val
+
+  io.result := Mux(io.enableFlipflop(0), optionalReg, lookupTable.io.res) // reg is enabled/disabled by enableFlipflop
+
+}
+
+class LogicBlockTests(c: LogicBlock) extends Tester(c) {
+  poke(c.io.programmedInputs, 0x8C8C)
+  poke(c.io.enableFlipflop, 1)
+  poke(c.io.flipflopReset, 0)
+  poke(c.io.reset, 0)
+
+  var valuesWithResults = Array(Array(0,0),Array(1,0),Array(2,1),Array(3,1),
+    Array(4,0),Array(5,0),Array(6,0),Array(7,1),Array(8,0),Array(9,0),Array(10,1),Array(11,1),
+    Array(12,0),Array(13,0),Array(14,0),Array(15,1))
+
+  for (valueWithResult <- valuesWithResults) {
+    poke(c.io.selectInputs, valueWithResult(0))
+    step(1)
+    expect(c.io.result, valueWithResult(1))
+  }
+}
+
+object LogicBlockTestRunner {
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(Array[String]("--backend", "c", "--compile", "--test", "--genHarness"),
+      () => Module(new LogicBlock()))
+    {
+      c => new LogicBlockTests(c)
+    }
+  }
+}
