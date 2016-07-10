67ea665c760a46f56421522fd04fd76a47d20522
JackDavidson <jack.davidson38@gmail.com>
2016-07-09 21:37:50 -0700

    Added support for multiplyer disable, and generalized routing table size

--- TritoncoreII/src/components/RoutingTable.scala
+++ TritoncoreII/src/components/RoutingTable.scala
@@ -2,21 +2,24 @@ import java.math.BigInteger
 
 import Chisel._
 
-class RoutingTable extends Module {
+class RoutingTable(inputCount : Int = 128, outputCount : Int = 220) extends Module {
+  val numberOfRoutingBits = outputCount * (scala.math.log(inputCount/4)/scala.math.log(2)).toInt
   val io = new Bundle {
-    val input      = Bits(INPUT,   128)
-    val routing    = Bits(INPUT,  1100)
-    val outputs    = Bits(OUTPUT,  220)
+    val input      = Bits(INPUT,  inputCount)
+    val routing    = Bits(INPUT,  numberOfRoutingBits)
+    val outputs    = Bits(OUTPUT, outputCount)
   }
 
   io.outputs := UInt(0) // weird CHISEL req
 
+
+  val groupingSize = outputCount / 4
   for (i <- 0 to 3) {    // low, med-low, med-high, high order bits
-    for (j <- 0 to 54) { // one for each LogicBlock on all orders
+    for (j <- 0 to (groupingSize - 1)) { // one for each LogicBlock on all orders
     val lut = Module(new LUT(inCount = 5,outCount = 1))
       lut.io.lut               := io.input((i + 1) * 32 - 1, i * 32) // take the high, med, or etc order bits
-      lut.io.sel               := io.routing((i * 55 + j + 1) * 5 - 1, (i * 55 + j) * 5)
-      io.outputs((i*55) + j)   := lut.io.res(0) // take logic block # j, and attach input i to it.
+      lut.io.sel               := io.routing((i * groupingSize + j + 1) * 5 - 1, (i * groupingSize + j) * 5)
+      io.outputs((i*groupingSize) + j)   := lut.io.res(0) // take logic block # j, and attach input i to it.
     }
   }
 }
