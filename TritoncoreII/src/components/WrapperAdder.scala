27c785e2991031f31f732cf83782579d8d753994
JackDavidson <jack.davidson38@gmail.com>
2016-07-09 17:43:29 -0700

    Finished Adder block, added tests, and fixed some bugs

--- TritoncoreII/src/components/WrapperAdder.scala
+++ TritoncoreII/src/components/WrapperAdder.scala
@@ -13,14 +13,22 @@ class WrapperAdder extends Module {
     val carryIn     = Bits(INPUT,  1)
     val inputA      = Bits(INPUT,  8) // the first input number
     val inputB      = Bits(INPUT,  8) // the second input number
+    val enableReg   = Bits(INPUT,  1)
+    val reset       = Bits(INPUT,  1)
     val result      = Bits(OUTPUT, 8) // the addition result
     val carryOut    = Bits(OUTPUT, 1)
   }
 
   // the following line is weird. We need a carry-in and a carry-out. Hopefully verilog will optomize this.
   val additionResult = UInt(0, width = 9) + io.inputA + io.inputB + io.carryIn
-  io.result := additionResult(7,0);
-  io.carryOut := additionResult(8);
+  val optionalRegResult = Reg(init = UInt(0, width = 8)) // reg on the output
+  val optionalRegCout = Reg(init = UInt(0, width = 1)) // reg on the output
+
+  optionalRegResult := Mux(io.reset(0), UInt(0, width = 8), additionResult(7,0))  // resets to 0
+  optionalRegCout := Mux(io.reset(0), UInt(0, width = 1), additionResult(8))       // resets to 0
+
+  io.result := Mux(io.enableReg(0), optionalRegResult, additionResult(7,0)) // reg is enabled/disabled by enableRe
+  io.carryOut := Mux(io.enableReg(0), optionalRegCout, additionResult(8))
 }
 
 class WrapperAdderTests(c: WrapperAdder) extends Tester(c) {
